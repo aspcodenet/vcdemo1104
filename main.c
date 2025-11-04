ebb4e34d4dc8d613d290e137a972d144f1ac9c23
Stefan Holmberg <stefan.holmberg@systementor.se>
2025-11-04 09:20:46 +0100

    v3

--- main.c
+++ main.c
@@ -8,6 +8,8 @@ typedef enum {
 } LANG; 
 
 // how NOT to do it:
+// INTE OK attr returnera en pelare toill en stackallokerad variabel!!!
+// just denna är också farlig för buffer overflow
 char *greet(const char *name, LANG lang) {
 
     char buffer[50];
@@ -21,7 +23,44 @@ char *greet(const char *name, LANG lang) {
     return buffer;
 }
 
+void greetGood(const char *name, LANG lang, char *buffer) {
+    if(lang == SE){
+        strcpy(buffer, "Hej, ");
+    }else{
+        strcpy(buffer, "Hello, "); 
+    }
+    strcat(buffer, name);
+    strcat(buffer, "!");
+    return buffer;
+}
+
+
+typedef enum {
+    ERRORS_NONE,
+    ERRORS_INVALID_LANG,
+    ERRORS_BUFFER_TO_SMALL
+} ERRORS;
+
+// DETTA ÄR OK!!!
+// string literals ligger i text segmentet
+char *getErrorText(ERRORS err) {
+    switch(err) {
+        case ERRORS_NONE:
+            return "No errors.";
+        case ERRORS_INVALID_LANG:
+            return "Invalid language.";
+        case ERRORS_BUFFER_TO_SMALL:
+            return "Buffer too small.";
+        default:
+            return "Unknown error.";
+    }
+}
+
+
 int main() {
+    char buffer[50];
+    greetGood("Stefan", EN, buffer);
+
     char *text = greet("Stefan", EN);
     printf("%s\n", text);
 
